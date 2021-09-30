228e61197bf4b7073a49fea23df171d0b6e6671c
simo044a <ssalquist@hotmail.dk>
2021-09-30 09:15:05 +0200

    første justeringer - Af Sif

---
+++ src/Adventure.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Adventure {
+
+
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    boolean isGameActive = false;
+    String playerDirection;
+
+    System.out.println("\nWelcome to the adventure game 1.0");
+
+
+    do {
+      System.out.println("\nChoose a direction to go: north, south, west and east\n");
+
+      playerDirection = in.nextLine();
+
+      if(playerDirection.equals("go north")){
+        System.out.println("Going north");
+      } else if(playerDirection.equals("go south")){
+        System.out.println("Going south");
+      } else if (playerDirection.equals("go west")){
+        System.out.println("Going west");
+      } else if(playerDirection.equals("go east")){
+        System.out.println("Going east");
+      } else if(playerDirection.equals("look")){
+        System.out.println("Looking around");
+      } else if(playerDirection.equals("exit")) {
+        isGameActive = true;
+      } else {
+        System.out.println("Sorry, what you typed in were invalid! Try again.");
+      }
+
+    } while(!isGameActive);
+
+  }
+
+}
---
+++ src/Room.java
@@ -0,0 +1,13 @@
+public class Room {
+
+  private String name;
+  private String roomDescription;
+  private boolean north; // (Jeg er stadig usikre på hvilke datatyper)
+  private boolean south;
+  private boolean west;
+  private boolean east;
+  private boolean look;
+
+
+
+}
