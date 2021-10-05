51e46f68c4781c8c4d17e5b2bd55d5da91164366
simo044a <91477508+simo044a@users.noreply.github.com>
2021-10-05 19:53:56 +0200

    Update Room.java
    Room beskrivlser opdateret

--- src/Room.java
+++ src/Room.java
@@ -7,11 +7,8 @@ public class Room {
   private Room west;
   private Room east;
 
-  // Constructor til at tildele name og roomDescription værdier til hvert room, ud fra setter
-  Room(String name, String roomDescription) {
-    setName(name);
-    setRoomDescription(roomDescription);
-  }
+  // Constructor til at tildele name til hvert rum ud fra setter.
+  Room(String name, String roomDescription) {setName(name);}
 
 
   // Set and Get for name
@@ -68,10 +65,6 @@ public class Room {
     return east;
   }
 
-
-  public String toString() {
-    return name + " " + roomDescription;
-
   }
 
   /*
@@ -109,7 +102,99 @@ public class Room {
       return true;}
   }
 
+  String room1Description = room1Text();
+  String room2Description = room2Text();
+  String room3Description = room3Text();
+  String room4Description = room4Text();
+  String room5Description = room5Text();
+  String room6Description = room6Text();
+  String room7Description = room7Text();
+  String room8Description = room8Text();
+  String room9Description = room9Text();
 
+  // tekst bokse room navne og beskrivelse
+  public String room1Text() {
+    return """
+        You wake up in a van. You go outside to check your surroundings.
+        You're at a road with trashed vehicle one way, the other way,
+        full of mutated creatures in the distant. The road goes
+        along a steep hill. Across the road, a big building.""";
+  }
+
+  public String room2Text() {
+    return """
+        Room 2:
+        Her er vest tilbage og øst er frem.
+        This area has a thick fog. Looking around, you notice some destroyed
+        and burned vehicles, and you can hear some kind of howls in the distance,
+        but it doesn't sound like wolfs or dogs, but bigger and more human.
+        The buildings in this area looks like they have been abandoned a long time ago.""";
+  }
+
+  public String room3Text() {
+    return """
+        Room 3: Skal kunne gå mod vest eller syd.
+        mod syd: the fog is fading a bit, the buildings here are destroyed.
+        The sky is filled with birds, looks like crows but kinda big. Maybe ravens?
+        There is only on way trough south, and that is a hole in the ground.
+        It looks like the road has collapsed down in to a old metro.""";
+  }
+
+  public String room4Text() {
+    return """
+        Room 4:
+        This area has broken and half burned shops and apartments. It was once filled with life
+        but it's dead and silent now. It looks like there is trash along the road but when you look closer,
+        you notice its only cloth, shoes and old suitcases looking like something from old soviet Russia.
+        The area gives you the feeling of dread and people most have been in a rush.
+        The shops a boarded shut and you are only able to keep walking the same path forward.""";
+  }
+
+  public String room5Text() {
+    return """
+        Room 5: Her kan man kun komme fra rum 8. Og det er north fra rum 8.
+        The elevator goes all the way up to the roof without giving you a chanceto pick any floor.
+        The elevator does open half way up, just enough for you to squeeze through.
+        The helicopter looks out of place, since it looks all new with no scratches or anything.
+        This is your way out of what ever this place is!""";
+  }
+
+  public String room6Text() {
+    return """
+        Room 6:
+        As soon as you jump down from the edge of the road hanging down from the upper level, you land in water.
+        It comes as a surprise because it was so still that it looked like dirt.
+        It's not that deep but reach up to your knees.
+        When you look up, you can see the birds through the hole, flying around, kinda fighting in the air.
+        But when you look around, you notice how dark it's actually is when you are down here.
+        The light from the hole you came from, shows that the metro is collapsed, making it only possible
+        to take one path from here, in to darkness.""";
+  }
+
+  public String room7Text() {
+    return """
+        Room 7: Dette sted kommer man fra north men kan kun gå videre øst.
+        You come to a road block. There have been a lot of military here, blocking the road.
+        The area have proof of combat. Bullet holes on the walls, the road have craters from grenades.
+        The buildings in this area is destroyed and the only thing that looks like it haven't seen
+        combat is the hospital in the distance, behind some of the roadblocks and military vehicles.""";
+  }
+
+  public String room8Text() {
+    return """
+        Room 8:
+        You are now standing inside of a hospital lounge. Around you is hospital beds, half emptied
+        first-aid kits and an empty vending machine with broken glass. You can also see an elevator
+        which seems to be working and over it you can see a sign stating that there is a
+        helipad on the top floor of the building.""";
+  }
+
+  public String room9Text() {
+    return """
+        room 9:
+        You are still surrounded by water up to your knees. A train stationed in front of you is lit up
+        by an opening further ahead which looks like the way back up to the surface.""";
+  }
 
 }
 
