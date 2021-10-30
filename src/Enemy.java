5336f09db99c553fbaeace1c2f9a00b19168883a
simo044a <91477508+simo044a@users.noreply.github.com>
2021-10-30 18:12:50 +0200

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,3 +1,12 @@
-public class Enemy {
+import java.util.ArrayList;
 
+public class Enemy extends Player {
+
+  private double enemyHealth;
+  private double enemyDamage;
+  private Room enemyCurrentRoom;
+
+  public Enemy(String playerName, ArrayList<Item> inventory, double health) {
+    super(playerName, inventory, health);
+  }
 }
