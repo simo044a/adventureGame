c32cfa16e03a1c3a0026ba3ba17e72ac1c4cfb59
simo044a <91477508+simo044a@users.noreply.github.com>
2021-10-31 14:13:42 +0100

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class Weapon extends Item{
 
   private double weaponDamage;
 
-  Weapon(String itemName, String itemDescription) {
-    super(itemName, itemDescription);
+  public void setWeaponDamage(double weaponDamage){this.weaponDamage = weaponDamage;}
+  public double getWeaponDamage(){return weaponDamage;}
+
+  Weapon(String itemName, String itemDescription, String itemType, double weaponDamage) {
+    super(itemName, itemDescription, itemType);
+    setWeaponDamage(weaponDamage);
   }
 }
